780c8c3b1791046d00edc5a1b625bbd37b831bdf
Amit Bajaj <amitbajaj@windowslive.com>
2016-07-07 17:45:48 +0300

    Initial version

---
+++ AccessToolKit/frmMain.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace AccessToolKit
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.helloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.areToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(559, 24);
+            this.menuStripMain.TabIndex = 1;
+            this.menuStripMain.Text = "menuStrip";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helloToolStripMenuItem,
+            this.howToolStripMenuItem,
+            this.areToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(125, 20);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // helloToolStripMenuItem
+            // 
+            this.helloToolStripMenuItem.Name = "helloToolStripMenuItem";
+            this.helloToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.helloToolStripMenuItem.Text = "Hello";
+            // 
+            // howToolStripMenuItem
+            // 
+            this.howToolStripMenuItem.Name = "howToolStripMenuItem";
+            this.howToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.howToolStripMenuItem.Text = "How";
+            // 
+            // areToolStripMenuItem
+            // 
+            this.areToolStripMenuItem.Name = "areToolStripMenuItem";
+            this.areToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.areToolStripMenuItem.Text = "Are";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 315);
+            this.Controls.Add(this.menuStripMain);
+            this.MainMenuStrip = this.menuStripMain;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "Access Tool Kit";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem helloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem areToolStripMenuItem;
+    }
+}
+
---
+++ AccessToolKit/frmMain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccessToolKit
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            BuildMenuItems();
+            
+        }
+        private void BuildMenuItems()
+        {
+            for (int i = 0; i < menuStripMain.Items.Count; i++) menuStripMain.Items.RemoveAt(0);
+            ToolStripMenuItem[] items = new ToolStripMenuItem[1];
+            ToolStripItem o;
+            items[0] = new ToolStripMenuItem("&File");
+            items[0].Name = "FileMenu";
+            o = items[0].DropDownItems.Add("Open");
+            o.Tag = "OpenDB";
+            o.Name = "OpenDB";
+            o.Click += new EventHandler(MenuItemClickHandler);
+            o = items[0].DropDownItems.Add("Create");
+            o.Tag="CreateDB";
+            o.Name = "CreateDB";
+            o.Click += new EventHandler(MenuItemClickHandler);
+            items[0].Click += new EventHandler(MenuItemClickHandler);
+            menuStripMain.Items.AddRange(items);
+        }
+
+        private void MenuItemClickHandler(object sender, EventArgs e)
+        {
+            ToolStripMenuItem clickedItem = (ToolStripMenuItem)sender;
+            if (clickedItem.Tag != null)
+            {
+                MessageBox.Show(clickedItem.Tag.ToString());
+
+            }
+
+        }
+    }
+}
